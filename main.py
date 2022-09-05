f17c4a3b5fb1ff7c9d2878d50fd666c288f5a7f2
chtholine <oleg123qwe@gmail.com>
2022-09-05 22:21:25 +0300

    Truth or Dare v1.0

--- main.py
+++ main.py
@@ -1,52 +1,54 @@
 import random
 
-players = []
-truth = ['How many plants you have?', 'Can you cook?', 'What is your favourite band?']
-dare = ['Jump as high as you can', 'Run around the house', 'Make a snack']
-random.shuffle(truth)
-random.shuffle(dare)
+if __name__ == "__main__":
 
+    class Game:
+        def __init__(self):
+            self.players = []
 
-def number_of_players():
-    choice = True
-    while True:
-        i = input("\nTo begin just press ENTER\n-------------------------\nPlayers name: ")
-        players.append(i)
-        if len(i) == 0:
-            players.pop(-1)
-            break
-    print(f'There are {len(players)} players.')
+        def number_of_players(self):
+            while True:
+                i = input("\nTo begin just press ENTER\n-------------------------\nPlayers name: ")
+                self.players.append(i)
+                if len(i) == 0:
+                    self.players.pop(-1)
+                    break
+            print(f'There are {len(self.players)} players.')
 
+        def gameplay(self):
+            truth = ['How many plants you have?', 'Can you cook?', 'What is your favourite band?']
+            dare = ['Jump as high as you can', 'Run around the house', 'Make a snack']
+            random.shuffle(truth)
+            random.shuffle(dare)
 
-def game():
-    play = True
-    while play:
-        for player in players:
-            # if lists are empty: stop
-            if not truth + dare:
-                play = False
-                print("\nIt seems we're out of questions!\nThanks for playing!")
-                break
+            play = True
+            while play:
+                for player in self.players:
+                    # if lists are empty: stop
+                    if not truth + dare:
+                        play = False
+                        print("\nIt seems we're out of questions!\nThanks for playing!")
+                        break
 
-            q1 = input(f"\n{player}, truth or dare? truth/dare ").lower()
-            if q1 == "truth":
-                print(truth.pop(0))
-                if not truth:
-                    print("\nWe're out of truth questions")
+                    q1 = input(f"\n{player}, truth or dare? truth/dare ").lower()
+                    if q1 == "truth":
+                        if not truth:
+                            print("\nWe're out of truth questions")
+                            break
+                        print(truth.pop(0))
 
-            if q1 == "dare":
-                print(dare.pop(0))
-                if not dare:
-                    print("\nWe're out of dare questions")
+                    if q1 == "dare":
+                        if not dare:
+                            print("\nWe're out of dare questions")
+                            break
+                        print(dare.pop(0))
 
+                    q2 = input("Do you want to continue? yes/no ")
+                    if q2 == 'no':
+                        play = False
+                        print("\nThanks for playing!")
+                        break
 
-
-            q2 = input("Do you want to continue? yes/no ")
-            if q2 == 'no':
-                play = False
-                print("\nThanks for playing!")
-                break
-
-
-number_of_players()
-game()
+    my_game = Game()
+    my_game.number_of_players()
+    my_game.gameplay()
