007a784707a948c927ea40a5dc3fd07c3f8d9294
chtholine <oleg123qwe@gmail.com>
2022-09-06 18:06:57 +0300

    Truth or Dare v1.0

--- main.py
+++ main.py
@@ -1,55 +1,54 @@
 import random
 
-if __name__ == "__main__":
 
-    class Game:
-        def __init__(self):
-            self.players = []
+class Game:
+    def __init__(self):
+        self.players = []
 
-        def number_of_players(self):
-            while True:
-                names = input("\nTo begin just press ENTER\n-------------------------\nPlayers name: ")
-                self.players.append(names)
-                if len(names) == 0:
-                    self.players.pop(-1)
+    def number_of_players(self):
+        while True:
+            names = input("\nTo begin just press ENTER\n-------------------------\nPlayers name: ")
+            self.players.append(names)
+            if len(names) == 0:
+                self.players.pop(-1)
+                break
+        if len(self.players) == 0:
+            exit("At least one player is needed!")
+        print(f'There are {len(self.players)} players.')
+
+    def gameplay(self):
+        truth = ['How many plants you have?', 'Can you cook?', 'What is your favourite band?']
+        dare = ['Jump as high as you can', 'Run around the house', 'Make a snack']
+        random.shuffle(truth)
+        random.shuffle(dare)
+        play = True
+        while play:
+            for player in self.players:
+                if not truth + dare:
+                    play = False
+                    print("\nIt seems we're out of questions!\nThanks for playing!")
                     break
-            if len(self.players) == 0:
-                exit("At least one player is needed!")
-            print(f'There are {len(self.players)} players.')
-
-        def gameplay(self):
-            truth = ['How many plants you have?', 'Can you cook?', 'What is your favourite band?']
-            dare = ['Jump as high as you can', 'Run around the house', 'Make a snack']
-            random.shuffle(truth)
-            random.shuffle(dare)
-
-            play = True
-            while play:
-                for player in self.players:
-                    if not truth + dare:
-                        play = False
-                        print("\nIt seems we're out of questions!\nThanks for playing!")
+                choice = input(f"\n{player}, truth or dare? truth/dare ").lower()
+                if choice in ['truth' or 't']:
+                    if not truth:
+                        print("\nWe're out of truth questions")
                         break
+                    print(truth.pop(0))
 
-                    choice = input(f"\n{player}, truth or dare? truth/dare ").lower()
-                    if choice == "truth":
-                        if not truth:
-                            print("\nWe're out of truth questions")
-                            break
-                        print(truth.pop(0))
-
-                    if choice == "dare":
-                        if not dare:
-                            print("\nWe're out of dare questions")
-                            break
-                        print(dare.pop(0))
-
-                    continue_q = input("Do you want to continue? yes/no ").lower()
-                    if continue_q == 'no':
-                        play = False
-                        print("\nThanks for playing!")
+                if choice in ['dare', 'd']:
+                    if not dare:
+                        print("\nWe're out of dare questions")
                         break
+                    print(dare.pop(0))
 
-    my_game = Game()
-    my_game.number_of_players()
-    my_game.gameplay()
+                continue_q = input("Do you want to continue? yes/no ").lower()
+                if continue_q in ['no', 'n']:
+                    play = False
+                    print("\nThanks for playing!")
+                    break
+
+
+if __name__ == "__main__":
+    main = Game()
+    main.number_of_players()
+    main.gameplay()
